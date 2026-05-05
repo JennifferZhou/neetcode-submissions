b821a83c8c5dd8e393fb34e8256bdc06311a6a42
JennifferZhou <jenniferlezhou@gmail.com>
2026-05-05 11:52:56 -0700

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            String s = strs[i];
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String newS = new String(arr);
+            res.putIfAbsent(newS, new ArrayList<String>());
+            List<String> match = res.get(newS);
+            match.add(s);
+        }
+        List<List<String>> rets = new ArrayList<> ();
+        for (List<String> list : res.values()) {
+            rets.add(list);
+        }
+        return rets;
+    }
+
+}

ce90fb0ac90379f668a879524d99d5142b23ad42
JennifferZhou <jenniferlezhou@gmail.com>
2026-06-04 16:55:27 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,30 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int top = stack.pop();
+        if (minStack.peek() == top) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}

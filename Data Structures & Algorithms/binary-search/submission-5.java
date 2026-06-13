e38efdeacf723e45def843cf6e08ed9024736a9b
JennifferZhou <jenniferlezhou@gmail.com>
2026-06-13 05:46:07 -0700

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+        while (l <= r) {
+            int index = (r+l)/2;
+            System.out.println(index);
+            if (nums[index] == target) return index;
+            else if (nums[index] > target) r--;
+            else l++;
+        }
+        return -1;
+    }
+}
